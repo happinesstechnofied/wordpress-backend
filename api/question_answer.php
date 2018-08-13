c8bf15ed36821664763d0136933fe27c36431cc8
Vishal Gandhi <vishal.gandhi71.86@gmail.com>
2018-08-13 17:39:19 +0530

    Api for serviceprovider
    Create Api for service provider question answer history display

--- api/question_answer.php
+++ api/question_answer.php
@@ -75,6 +75,87 @@ try {
 			}
 		
 		break;
+		
+		case "get_ques_history_sp":
+			if(isset($_REQUEST['user_id']))
+			{
+				$user_id = $_POST['user_id'];
+
+				$args = array( 'post_type' => 'services', 'posts_per_page' => -1 ,'post_status' => array( 'pending', 'publish' ),'author' => $user_id );
+				
+				query_posts($args);
+
+				global $post;
+
+				$data_questions = array();
+				while (have_posts()) : the_post();
+					$postdata = $post;
+					$post_id=$postdata->ID;
+					
+					//get question ans history for service provider
+					$args = array(
+						'post_id' => $post_id,
+						'parent'  => 0,
+						'orderby' => 'comment_date',
+						'order' => 'ASC',
+					);
+					
+					$questions_data = array();
+					
+					$questions = get_comments($args);
+
+					foreach($questions as $question){
+						$temp_question_data = array();
+						$temp_question_data['question_id'] = $question->comment_ID;
+						$temp_question_data['service_id'] = $question->comment_post_ID;
+						$temp_question_data['question'] = $question->comment_content;
+						$temp_question_data['date'] = $question->comment_date;
+						
+						$args = array(
+							'parent'  => $question->comment_ID,
+							'orderby' => 'comment_date',
+							'order' => 'ASC',
+						);
+						
+						$answers = get_comments($args);
+						
+						foreach($answers as $answer){
+							$temp_answer_data = array();
+							$temp_answer_data['answer_id'] = $answer->comment_ID;
+							$temp_answer_data['service_id'] = $answer->comment_post_ID;
+							$temp_answer_data['answer'] = $answer->comment_content;
+							$temp_answer_data['date'] = $answer->comment_date;
+							
+							$temp_question_data['answers'][] = $temp_answer_data;
+						}
+						
+						
+						$data_questions[] = $temp_question_data;
+						
+					}
+				
+				endwhile;
+
+				// Reset Query
+				wp_reset_query();
+						
+				$status = 1;
+				$data['status'] = ($status > 1) ? 'failed' : 'success';
+				$data['message'] = "Sign in sucessfully";
+				$data['que_ans_history'] = $data_questions;
+				echo json_encode($data);
+				die;			
+				
+			}else{
+				$status = 2;
+				$data['status'] = ($status>1)? "fail" : "success";
+				$data['message'] = "Please provide valid details";
+				echo json_encode($data);
+				die;
+			}
+		
+		break;
+		
 		case "get_service_ques":
 		
 			if(isset($_REQUEST['service_id']))
